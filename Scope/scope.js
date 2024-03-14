fa55c1f771317b16420c6e80a12458bb50393810
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-03-15 01:51:07 +0300

    Update scope.js

---
+++ Scope/scope.js
@@ -0,0 +1,21 @@
+var sayi =10;
+var sehir ="Ankara";
+
+sayi=11;
+sehir="Istanbul";
+
+var sehir="Adana";
+
+let il ="Ankara"
+il="Bursa";
+// let il ="İzmir"
+
+function mesajVer(){
+    console.log(sehir);
+}
+
+// console.log(sayi);
+// console.log(sehir);
+// console.log(il);
+
+mesajVer();
